9dfdc7121895be22bb06cdcc0698dfe008ef758c
Shaik Atiq <shaikatheeq4@gmail.com>
2026-08-16 22:11:28 +0530

    organize: remove old root operators exercise

--- logic_operators.py
+++
@@ -1,8 +0,0 @@
-weight = input("Weight: ")
-unit = input("(L)bs or (K)g: ")
-if unit.upper() == "L":
-    convert = float(weight) * 0.45
-    print(f"You are {convert} kilos")
-else:
-    convert = float(weight) // 0.45
-    print(f"You are {convert} pounds")
